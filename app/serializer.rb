4354970d8e2c3e1c1ea740d5981a8c272d19e99a
Sumit <sumit.18.paul@gmail.com>
2022-09-13 18:49:08 +0000

    serializer added

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,35 @@
 class Serializer
+    @@obj_attributes ||= {}
+    @@obj ||= []
+  
+    def initialize(val)
+        @@obj = val
+        # p @@obj_attributes[self.class]
+        @serialize_hash = @@obj_attributes[self.class]
+        @serialize_hash.each do |key, value|
+            if value
+                @serialize_hash[key] = value.call()
+            else
+                @serialize_hash[key] = @@obj[key]
+            end
+        end
+    end
+
+    def serialize
+        @serialize_hash
+    end
+  
+    def self.object
+        @@obj
+    end
+  
+    def self.attribute(value, &block)
+        @@obj_attributes[self] ||= {}
+        if block_given?
+            @@obj_attributes[self][value] = block
+        else
+            @@obj_attributes[self][value] = nil
+        end 
+        # p "#{@@obj_attributes[self]}"
+    end
 end
